33d2b019a76be8d7be271b3e866ccc75544bf7cc
213fumiya <21311232fn@tama.ac.jp>
2016-04-20 03:32:12 +0900

    photon

---
+++ Assets/Scripts/manage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class manage : Photon.MonoBehaviour {
+	private bool keyLock;
+	private GameObject mySyncObj;
+	// Use this for initialization
+	void Start () {
+		keyLock = false;
+		PhotonNetwork.ConnectUsingSettings (null);
+	}
+	void OnJoinedLobby(){
+		Debug.Log ("test");
+		PhotonNetwork.JoinRandomRoom ();
+	}
+	void OnJoinedRoom(){
+		Debug.Log ("success!!");
+		keyLock = true;
+		float x = Random.Range (-5.0f, 5.0f);
+		float y = Random.Range (-5.0f, 5.0f);
+		float z = Random.Range (0.0f, 5.0f);
+		mySyncObj =	PhotonNetwork.Instantiate("Cube",new Vector3(x,y,z),Quaternion.identity,0);
+		if (mySyncObj == null)
+			return;
+	}
+	void OnPhotonRandomJoinFailed(){
+		Debug.Log ("Room is Not Found");
+		PhotonNetwork.CreateRoom("test");
+	}
+	void FixedUpdate(){
+		if (mySyncObj != null) {
+			Vector3 SyncVec = mySyncObj.transform.position;
+			SyncVec.y += Input.GetAxis ("Vertical")/2;
+			SyncVec.x += Input.GetAxis ("Horizontal")/2;
+			mySyncObj.transform.position = SyncVec;
+		}
+	}
+
+}
---
+++ Assets/Scripts/objPos.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objPos :Photon.MonoBehaviour{
+	Vector3 SyncVec;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!photonView.isMine) {       // photonViewが自分自身ではない場合、位置と回転を反映.
+			transform.position =Vector3.Slerp (this.transform.position,SyncVec,Time.deltaTime * 5);
+		}
+	}
+
+	
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) {
+			// 自分のプレイヤー情報を送信.
+			stream.SendNext(transform.position);
+		} else {
+			// 他のプレイヤー情報を受信.
+			SyncVec = (Vector3)stream.ReceiveNext();
+		}
+	}
+}
